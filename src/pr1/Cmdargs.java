02559484e5cffa815292dd50ba9dac125e59cb74
Sowfny <sivtsovasofia@gmail.com>
2022-09-18 21:56:52 +0300

    add comment

--- src/pr1/Cmdargs.java
+++ src/pr1/Cmdargs.java
@@ -2,6 +2,7 @@ package pr1;
 
 public class Cmdargs {
     public static void main(String[] args) {
+        //  java -classpath C:\Users\sonas\IdeaProjects\Practice-Java\out\production\HWjava pr1.Cmdargs  shefgsjef
         for (String s : args) {
             System.out.println(s);
         }
