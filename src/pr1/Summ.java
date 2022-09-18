4cbaaf3ec510d01ab64ef8b2a3cbe116024221bc
Sowfny <sivtsovasofia@gmail.com>
2022-09-18 21:40:43 +0300

    commit with another branch

--- src/pr1/Summ.java
+++ src/pr1/Summ.java
@@ -3,16 +3,12 @@ import java.util.Scanner;
 
 public class Summ {
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
-
         String[] numbers = input.nextLine().split(" ");
         int sum = 0;
         for (String number : numbers) {
-
             sum += Integer.parseInt(number);
         }
-
         System.out.println("The sum of the numbers: " + sum);
     }
 }
